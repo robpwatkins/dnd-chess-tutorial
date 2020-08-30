550c0d2c2cef5dccd4547144310b432da176b4f8
robpwatkins <robpwatkins@gmail.com>
2020-08-30 09:00:55 -0500

    workin on teh board

--- src/components/Board.js
+++ src/components/Board.js
@@ -2,6 +2,12 @@ import React from 'react';
 import Square from './Square';
 import Knight from './Knight';
 
+function renderSquare(i, [knightX, knightY]) {
+    const x = i % 8;
+    const y = Math.floor(i / 8);
+    const isKnightHere
+}
+
 const Board = () => {
     return (
         <div>
